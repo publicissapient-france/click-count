5c075e4363b97d6ec7369289e06ceeaba4dadc88
Cédric Souchon <capuchon.dev@gmail.com>
2018-06-04 11:08:47 +0200

    Change: Makes the web app configurable at runtime. The Redis host address and port can now be configured at run time through two env vars.

--- src/main/java/fr/xebia/clickcount/Configuration.java
+++ src/main/java/fr/xebia/clickcount/Configuration.java
@@ -10,8 +10,29 @@ public class Configuration {
     public final int redisConnectionTimeout;  //milliseconds
 
     public Configuration() {
-        redisHost = "redis";
-        redisPort = 6379;
-        redisConnectionTimeout = 2000;
+        String _redisHost = "redis";
+        int    _redisPort = 6379;
+
+        try {
+            String envVarStr;
+
+            // Get the redis host address in an env var is it exists,
+            // keep the default value otherwise.
+            envVarStr = System.getenv( "XEBIA_CLICK_COUNT_REDIS_HOST" );
+            if ( envVarStr != null )
+                _redisHost = envVarStr;
+
+            // Get the redis host port in an env var is it exists,
+            // keep the default value otherwise.
+            envVarStr = System.getenv( "XEBIA_CLICK_COUNT_REDIS_PORT" );
+            if ( envVarStr != null )
+                _redisPort = Integer.parseUnsignedInt( envVarStr );
+        } catch ( Exception e ) {
+        }
+        finally {
+            redisHost = _redisHost;
+            redisPort = _redisPort;
+            redisConnectionTimeout = 2000;
+        }
     }
 }
